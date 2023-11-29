8b9b48cb6aeafe7a5933a54012252c3b8ca5d9c4
Xiaoxianyue <2310219843@qq.com>
2023-11-29 17:37:47 +0800

    delete

--- src/.vuepress/sidebar/en.ts
+++ src/.vuepress/sidebar/en.ts
@@ -1,22 +1,5 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export const enSidebar = sidebar({
-  "/": [
-    "",
-    {
-      text: "Demo",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
-    },
-    {
-      text: "Articles",
-      icon: "book",
-      prefix: "posts/",
-      children: "structure",
-    },
-    "intro",
-    "slides",
-  ],
+  "/": "structure"
 });
